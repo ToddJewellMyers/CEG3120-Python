06e6bff65005fb74382fdb9278a1c1b53ebc06b1
Jewell Myers <64042050+ToddJewellMyers@users.noreply.github.com>
2022-11-08 07:45:01 -0500

    Add files via upload

---
+++ ceg3120-ToddJewellMyers/ceg3120-ToddJewellMyers2/Discord-Bot/bot.py
@@ -0,0 +1,49 @@
+import os
+
+import discord
+import random
+from dotenv import load_dotenv
+
+load_dotenv()
+#print(os.getenv('DISCORD_TOKEN'))
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    Navi_quotes = [ 
+        'Hey, Listen!', 'Hey!', 'Watch Out!' 
+    ]
+   
+    if message.content == 'navi':
+        response = random.choice(Navi_quotes)
+        await message.channel.send(response)
+
+client.run(TOKEN)
